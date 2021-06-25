f769dd0cb0751c67242983db60d70dc78440f122
Diksha katna <84858977+Diksha3012@users.noreply.github.com>
2021-06-25 13:55:26 +0530

    Better calculator

---
+++ building-better-calculator/Building better caluclator.py
@@ -0,0 +1,16 @@
+num1=float(input("Enter a number:"))
+op=input("Enter operator:")
+num2=float(input("Enter another number:"))
+
+if op == "+":
+    print(num1+num2)
+elif op =="-":
+    print(num2-num1)
+elif op == "/":
+    print(num2/num1)
+elif op == "*":
+    print(num1*num2)
+else:
+    print("Please enter a valid operator")
+
+
---
+++ building-better-calculator/README.md
@@ -0,0 +1,1 @@
+## Perfomed basic BODMAS 
