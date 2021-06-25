728c966109ba0e5b4df4db19dd2d7524d82322f9
Diksha katna <84858977+Diksha3012@users.noreply.github.com>
2021-06-25 14:12:56 +0530

    For loop

---
+++ for-loop/README.md
@@ -0,0 +1,1 @@
+## For loop working with example.
---
+++ for-loop/for loop.py
@@ -0,0 +1,17 @@
+friends =["kiran","jyoti","diksha"]
+
+for friend in  friends:
+    print(friend)
+
+for letter in "Diksha katna":
+    print(letter)
+
+for index in range(10):
+    print(index)
+
+for index in range(3,10):
+    print(index)
+
+list1 =["kiran","jyoti","diksha"]
+for index in range(len(list1)):
+     print(list1[index])
