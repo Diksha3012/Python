4079b60ad615fb69bee49a7a617991eada001f37
Diksha katna <84858977+Diksha3012@users.noreply.github.com>
2021-06-25 14:34:03 +0530

    String Function

---
+++ string-functions/string-function.py
@@ -0,0 +1,14 @@
+phrase ="Learning Python"
+
+
+print(phrase+" is cool")
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("L"))
+print(phrase.index("Python"))
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(phrase.replace("Python","Python Language"))
+print("learnig\npython")
