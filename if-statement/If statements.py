54efa70a90d5336cbdedcc7aa1444b4002761eaa
Diksha katna <84858977+Diksha3012@users.noreply.github.com>
2021-06-25 14:22:00 +0530

    Nested if else

---
+++ if-statement/If statements.py
@@ -0,0 +1,27 @@
+is_male = False
+is_tall= True
+
+if is_male or is_tall:
+    print("You are a male and tall or both")
+
+else:
+    print("you are nor male nor tall")
+
+is_fat =True
+is_cute=True
+if is_fat and is_cute:
+    print("You are my favourite")
+else:
+    print("You are not my friend")
+
+
+is_feminist=True
+is_genuine =False
+if is_feminist and is_genuine:
+    print(" you are a hero of nation")
+elif is_feminist and not (is_genuine):
+    print("Feminist are enough")
+elif not (is_feminist) and is_genuine:
+    print("Genuine is more authentic")
+else:
+    print("Mercy on you")
