f4de395ff5e31e321aadbb468ce9c8864e911db5
Diksha katna <84858977+Diksha3012@users.noreply.github.com>
2021-06-25 13:50:48 +0530

    Maths operation

---
+++ basic-math/basicmath.py
@@ -0,0 +1,9 @@
+my_number = 34
+print(my_number)
+print(str(my_number)+  " my favorite number")
+my_num = -10
+print(abs(my_num))
+print(pow(3,2))
+print(max(4,6))
+print(min(4,6))
+print(round(3.2))
